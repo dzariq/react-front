04b78ca352966eaaf8d0abda1e1a80fc528893a2
dzariq mirza <dzariq85@gmail.com>
2023-10-28 10:13:45 +0800

    fdfd

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -3,7 +3,7 @@ const User = db.user;
 
 exports.addUser = (req, res) => {
     User.create({
-        user_id: 333,
+        user_id: Math.floor(Math.random()*90000) + 10000,
         name: req.body.name
     });
 
